7dfda0981afaabd53c2a7f41905a197c2707abd1
Julius <berzinskas.julius@gmail.com>
2025-12-29 15:54:39 +0200

    sukurta Fake duomenu base

--- my_app/app/Http/Requests/ConferenceRequest.php
+++ my_app/app/Http/Requests/ConferenceRequest.php
@@ -6,23 +6,20 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ConferenceRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'title' => ['required', 'string', 'min:3'],
+            'description' => ['required', 'string', 'min:5'],
+            'speakers' => ['required', 'string', 'min:3'],
+            'date' => ['required', 'date'],
+            'time' => ['required'],
+            'address' => ['required', 'string', 'min:3'],
         ];
     }
 }
